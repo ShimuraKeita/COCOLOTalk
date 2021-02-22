10d45ce6faf018dfc2fcbc5c33f4e41481913436
ShimuraKeita <keitagsk8001@gmail.com>
2021-02-22 23:41:12 +0900

    LoginViewController再コネクト、　ログイン・新規登録画面のアニメーション作成

--- talk/LoginViewController.swift
+++ talk/LoginViewController.swift
@@ -21,10 +21,19 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var signUpButtonOutlet: UIButton!
     @IBOutlet weak var resendEmailButtonOutlet: UIButton!
     
+    //Label
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var signUpLabel: UILabel!
+    
+    //MARK: - Vars
+    var isLogin = true
+    
     //MARK: View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         loginButtonOutlet.layer.cornerRadius = 5
+        updateUIFor(login: true)
     }
     
     //MARK: - IBActions
@@ -40,7 +49,19 @@ class LoginViewController: UIViewController {
         
     }
     
-    @IBAction func signUpButtonPressed(_ sender: Any) {
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        updateUIFor(login: sender.titleLabel?.text == "ログイン")
+        isLogin.toggle()
+    }
+    
+    //MARK: - Animations
+    private func updateUIFor(login: Bool) {
         
+        loginButtonOutlet.setTitle(login ? "ログイン" : "新規作成", for: .normal)
+        signUpLabel.text = login ? "新規アカウント作成は" : "既にアカウントをお持ちの方は"
+        
+        UIView.animate(withDuration: 0.5) {
+            self.repeatPasswordTextField.isHidden = login
+        }
     }
 }
